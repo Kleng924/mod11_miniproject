d2388394dd2f39579257b00b8478d2965287e329
Kleng924 <168580002+Kleng924@users.noreply.github.com>
2025-02-03 20:25:21 -0800

    Update customerService.js

--- src/services/customerService.js
+++ src/services/customerService.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_URL = 'https://your-backend-api.com/customers';
+const API_URL = 'http://localhost:5000';
 
 // Fetch all customers
 export const getCustomers = async () => {
